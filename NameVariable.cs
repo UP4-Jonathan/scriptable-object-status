06065563a4a6c83a4b50090651acae6bf032e56f
UP4-Jonathan <84469189+UP4-Jonathan@users.noreply.github.com>
2021-05-19 14:54:31 -0400

    Delete NameVariable.cs

--- NameVariable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName="New Name Variable", menuName="Tools/New Name Variable")]
-public class NameVariable : ScriptableObject
-{
-   public string Value {get{return this.name;}}
-}
